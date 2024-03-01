23bec3d9104fbed0491b524152b51ade80cfd2e3
eleanorlurgio <eleanorlurgio7.com>
2024-03-01 22:17:12 +0000

    display compute metrics

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import evaluate_model
 import example_model
-import model
+import roberta_model
 
 def main():
 
-    # model.main()
-    evaluate_model.gender()
+    roberta_model.main()
+    # evaluate_model.gender()
     # evaluate_model.gender_emotion()
 
 if __name__ == "__main__":
